a4893a29bd51c994e72ace355f735c20532f20c7
chang <chang@nextbillion.ai>
2024-10-14 11:37:06 +0900

    feat: minor refactor on the export

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import * as fs from './fs.js'
 import * as shell from './shell.js'
-import { Operation as OperatorOperation } from './operator/operation.js'
-import * as OperatorResource from './operator/resource.js'
-import * as OperatorAsset from './operator/asset.js'
-import * as OperatorCtx from './operator/context.js'
-import * as OperatorUtil from './operator/util.js'
+import { Operation } from './operator/operation.js'
+import * as Resource from './operator/resource.js'
+import * as Asset from './operator/asset.js'
+import * as Context from './operator/context.js'
+import * as Util from './operator/util.js'
 export {
   fs,
   shell,
-  OperatorOperation,
-  OperatorResource,
-  OperatorAsset,
-  OperatorCtx,
-  OperatorUtil
+  Operation,
+  Resource,
+  Asset,
+  Context,
+  Util
 }
